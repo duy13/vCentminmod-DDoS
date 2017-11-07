8643d769d738b3debd1322330d166448dc177002
root <root@vdvesta-vananh.c.agile-entry-168703.internal>
2017-11-07 19:57:49 +0700

    duy commit

---
+++ README.md
@@ -0,0 +1,134 @@
+vCentminmod DDoS
+===================
+
+A small shell script auto Deploy and Use "Testcookie Module" for your Centminmod.
+
+Homepage: http://vddos.voduy.com
+
+Centminmod from: https://centminmod.com/install.html
+
+Testcookie module from: https://github.com/kyprizel/testcookie-nginx-module
+
+
+----------
+
+Install:
+-------------
+
+1. Install Centminmod: https://centminmod.com/install.html
+
+```
+yum -y update; curl -O https://centminmod.com/installer.sh && chmod 0700 installer.sh && bash installer.sh
+ 
+```
+
+2. Install vCentminmod Antiddos Addon: 
+
+```
+curl -O https://github.com/duy13/vCentminmod-DDoS/raw/master/vCentminmod-DDoS.sh && chmod 700 vCentminmod-DDoS.sh && bash vCentminmod-DDoS.sh
+
+```
+
+3. Re-compile Centminmod Nginx: 
+
+```
+cd /usr/local/src/centminmod 
+./centmin.sh
+
+```
+```
+"Option 4" to update config: "4).  Nginx Upgrade / Downgrade"
+```
+
+
+
+Check: 
+-------------
+
+```
+[root@vDDoS ~]# nginx -V
+
+nginx version: nginx/1.x.x
+built by clang 3.4.2 (tags/RELEASE_34/dot2-final)
+built with LibreSSL 2.5.5
+TLS SNI support enabled
+configure arguments: --with-ld-opt='-lrt -ljemalloc -Wl,-z,relro' --with-cc-opt='-m64 -mtune=native -g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wno-sign-compare -Wno-string-plus-int -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-const-variable -Wno-conditional-uninitialized -Wno-mismatched-tags -Wno-c++11-extensions -Wno-sometimes-uninitialized -Wno-parentheses-equality -Wno-tautological-compare -Wno-self-assign -Wno-deprecated-register -Wno-deprecated -Wno-invalid-source-encoding -Wno-pointer-sign -Wno-parentheses -Wno-enum-conversion' --sbin-path=/usr/local/sbin/nginx --conf-path=/usr/local/nginx/conf/nginx.conf --with-http_ssl_module --with-http_v2_module --with-http_gzip_static_module --add-module=../testcookie-nginx-module-master --with-http_stub_status_module --with-http_sub_module --with-http_addition_module --with-http_image_filter_module --with-http_secure_link_module --with-http_realip_module --with-http_geoip_module --with-openssl-opt=enable-tlsext --add-module=../ngx-fancyindex-ngx-fancyindex --add-module=../ngx_cache_purge-2.3 --add-module=../nginx-http-concat-master --add-module=../memc-nginx-module-0.17 --add-module=../srcache-nginx-module-0.31 --add-module=../ngx_devel_kit-0.3.0 --add-module=../set-misc-nginx-module-0.31 --add-module=../echo-nginx-module-0.60 --add-module=../redis2-nginx-module-0.13 --add-module=../ngx_http_redis-0.3.7 --add-module=../nginx_upstream_check_module-0.3.0 --add-module=../nginx-module-vts --add-module=../headers-more-nginx-module-0.33 --with-openssl=../libressl-2.5.5 --with-libatomic --with-threads --with-stream --with-stream_ssl_module --with-pcre=../pcre-8.41 --with-pcre-jit
+
+```
+```
+Option "--add-module=../testcookie-nginx-module-master" is displayed.
+```
+
+----------
+
+
+Using:
+-------------
+1. Add your website in Centminmod: https://centminmod.com/getstarted.html#2
+
+Example, I added new domain called your-domain.com:
+
+```
+Nginx vhost conf path will be at /usr/local/nginx/conf/conf.d/your-domain.com.conf
+Nginx SPDY SSL vhost conf path will be at /usr/local/nginx/conf/conf.d/your-domain.com.ssl.conf
+Nginx Self-Signed SSL Certificate Directory at /usr/local/nginx/conf/ssl/your-domain.com
+Vhost public web root will be at /home/nginx/domains/your-domain.com/public
+Vhost log directory will be at /home/nginx/domains/your-domain.com/log 
+```
+
+2. Edit your website config (vhost file):
+```
+nano /usr/local/nginx/conf/conf.d/your-domain.com.conf
+
+...
+  root /home/nginx/domains/your-domain.com/public;
+
+  # prevent access to ./directories and files
+  location ~ (?:^|/)\. {
+   deny all;
+  }
+ include /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf;
+  location / {
+ include /usr/local/nginx/conf/vCentminmod-DDoS/on.conf;
+ 
+# block common exploits, sql injections etc
+#include /usr/local/nginx/conf/block.conf;
+
+  # Enables directory listings when index file not found
+  #autoindex  on;
+
+  # Shows file listing times as local time
+  #autoindex_localtime on;
+
+  # Enable for vBulletin usage WITHOUT vbSEO installed
+  # More example Nginx vhost configurations at
+  # http://centminmod.com/nginx_configure.html
+  #try_files    $uri $uri/ /index.php;
+
+  }
+...
+```
+
+Include config file in folder */usr/local/nginx/conf/vCentminmod-DDoS*
+```
+Include 5s.conf or high.conf to select the Security Mode.
+Include on.conf or off.conf to turn on or off the Security Mode.
+```
+
+3. Restart Nginx:
+
+```
+service nginx restart
+```
+
+Website: 
+-------------
+
+![vCentminmod Screenshot](https://lh5.googleusercontent.com/-HDS7MZ3jLrs/WgGnnGzTRCI/AAAAAAAABvs/htWtSvQVglE6tRQX-g5UNie-bcB9fLmugCLcBGAs/s1600/vCentminmod-DDoS.png "vCentminmod DDoS Screenshot 1")
+
+More:
+---------------
+Document: http://vddos.voduy.com
+```
+Still in beta, use at your own risk! It is provided without any warranty!
+```
---
+++ vCentminmod-DDoS.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+# vCentminmod Antiddos Addon (vddos.voduy.com)
+# Thanks you for using!
+
+if [ ! -f /etc/redhat-release ]; then
+	echo 'ERROR! Recommend use CentOS Linux release 7 x86_64!
+	'
+	exit 0
+fi
+
+if [ `arch` != "x86_64" ]; then
+	echo 'ERROR! Recommend use CentOS Linux release 7 x86_64!
+	'
+	exit 0
+fi
+
+if [ $(id -u) != "0" ]; then
+	echo 'ERROR! Please "su root" and try again!
+	'
+	exit 0
+fi
+
+if [ ! -f /usr/local/nginx/conf/nginx.conf ]; then
+	echo 'ERROR! Please install Centmin Mod before installing vCentminmod Addon!
+	'
+	exit 0
+fi
+
+ran_it=`cat /dev/urandom | tr -cd 'A-Z0-9' | head -c 5`
+ran_nhieu=`cat /dev/urandom | tr -cd 'a-z0-9' | head -c 32`
+
+clear
+############################################################
+echo '	Welcome to vCentminmod Antiddos Addon:
+A small shell script auto Deploy and Use "Testcookie Module" for your Centmin Mod.
+								Thanks you for using!
+
+
+
+'
+echo -n 'Press Enter to continue the installation: '
+read Enter
+
+echo '
+
+	Start the installation...
+
+'
+
+
+mkdir -p /usr/local/nginx/conf/vCentminmod-DDoS/html
+
+curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/aes.min.js.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/aes.min.js --silent
+goc=`curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/md5sum.txt --silent | grep "aes.min.js.txt" |awk 'NR==1 {print $1}'`
+tai=`md5sum /usr/local/nginx/conf/vCentminmod-DDoS/html/aes.min.js | awk 'NR==1 {print $1}'`
+if [ "$goc" != "$tai" ]; then
+	rm -rf /usr/local/nginx/conf/vCentminmod-DDoS/html/aes.min.js
+	curl -L https://3.voduy.com/vCentminmod-DDoS/aes.min.js.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/aes.min.js --silent
+fi
+
+curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/vddosw3data.js.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/vddosw3data.js --silent
+goc=`curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/md5sum.txt --silent | grep "vddosw3data.js.txt" |awk 'NR==1 {print $1}'`
+tai=`md5sum /usr/local/nginx/conf/vCentminmod-DDoS/html/vddosw3data.js | awk 'NR==1 {print $1}'`
+if [ "$goc" != "$tai" ]; then
+	rm -rf /usr/local/nginx/conf/vCentminmod-DDoS/html/vddosw3data.js
+	curl -L https://3.voduy.com/vCentminmod-DDoS/vddosw3data.js.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/vddosw3data.js --silent
+fi
+
+
+if [ ! -f /usr/local/nginx/conf/vCentminmod-DDoS/html/captcha.html ]; then
+	curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/html/captcha.html.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/captcha.html --silent
+	goc=`curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/md5sum.txt --silent | grep "captcha.html.txt" |awk 'NR==1 {print $1}'`
+	tai=`md5sum /usr/local/nginx/conf/vCentminmod-DDoS/html/captcha.html | awk 'NR==1 {print $1}'`
+	if [ "$goc" != "$tai" ]; then
+		rm -rf /usr/local/nginx/conf/vCentminmod-DDoS/html/captcha.html
+		curl -L https://3.voduy.com/vCentminmod-DDoS/html/captcha.html.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/captcha.html --silent
+	fi
+	cp /usr/local/nginx/conf/vCentminmod-DDoS/html/captcha.html /usr/local/nginx/conf/vCentminmod-DDoS/html/captcha.html.default
+fi
+
+if [ ! -f /usr/local/nginx/conf/vCentminmod-DDoS/html/5s.html ]; then
+	curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/html/5s.html.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/5s.html --silent
+	goc=`curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/md5sum.txt --silent | grep "5s.html.txt" |awk 'NR==1 {print $1}'`
+	tai=`md5sum /usr/local/nginx/conf/vCentminmod-DDoS/html/5s.html | awk 'NR==1 {print $1}'`
+	if [ "$goc" != "$tai" ]; then
+		rm -rf /usr/local/nginx/conf/vCentminmod-DDoS/html/5s.html
+		curl -L https://3.voduy.com/vCentminmod-DDoS/html/5s.html.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/5s.html --silent
+	fi
+	cp /usr/local/nginx/conf/vCentminmod-DDoS/html/5s.html /usr/local/nginx/conf/vCentminmod-DDoS/html/5s.html.default
+fi
+
+if [ ! -f /usr/local/nginx/conf/vCentminmod-DDoS/html/cookies.html ]; then
+	curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/html/cookies.html.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/cookies.html --silent
+	goc=`curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/md5sum.txt --silent | grep "cookies.html.txt" |awk 'NR==1 {print $1}'`
+	tai=`md5sum /usr/local/nginx/conf/vCentminmod-DDoS/html/cookies.html | awk 'NR==1 {print $1}'`
+	if [ "$goc" != "$tai" ]; then
+		rm -rf /usr/local/nginx/conf/vCentminmod-DDoS/html/cookies.html
+		curl -L https://3.voduy.com/vCentminmod-DDoS/html/cookies.html.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/html/cookies.html --silent
+	fi
+fi
+
+
+if [ ! -f /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf ]; then
+	curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/5s.conf.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf --silent
+	goc=`curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/md5sum.txt --silent | grep "5s.html.txt" |awk 'NR==1 {print $1}'`
+	tai=`md5sum /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf | awk 'NR==1 {print $1}'`
+	if [ "$goc" != "$tai" ]; then
+		rm -rf /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf
+		curl -L https://3.voduy.com/vCentminmod-DDoS/5s.conf.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf --silent
+	fi
+	s='%testcookie_secret%' ; r="$ran_nhieu"
+	sed -i "s#$s#$r#g" /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf
+	cp /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf /usr/local/nginx/conf/vCentminmod-DDoS/5s.conf.default
+fi
+
+if [ ! -f /usr/local/nginx/conf/vCentminmod-DDoS/high.conf ]; then
+	curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/high.conf.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/high.conf --silent
+	goc=`curl -L https://raw.githubusercontent.com/duy13/vCentminmod-DDoS/master/md5sum.txt --silent | grep "5s.html.txt" |awk 'NR==1 {print $1}'`
+	tai=`md5sum /usr/local/nginx/conf/vCentminmod-DDoS/high.conf | awk 'NR==1 {print $1}'`
+	if [ "$goc" != "$tai" ]; then
+		rm -rf /usr/local/nginx/conf/vCentminmod-DDoS/high.conf
+		curl -L https://3.voduy.com/vCentminmod-DDoS/high.conf.txt -o /usr/local/nginx/conf/vCentminmod-DDoS/high.conf --silent
+	fi
+	s='%testcookie_secret%' ; r="$ran_nhieu"
+	sed -i "s#$s#$r#g" /usr/local/nginx/conf/vCentminmod-DDoS/high.conf
+	cp /usr/local/nginx/conf/vCentminmod-DDoS/high.conf /usr/local/nginx/conf/vCentminmod-DDoS/high.conf.default
+fi
+
+
+cd /svr-setup
+rm -rf testcookie-nginx-module-master
+curl -L https://github.com/kyprizel/testcookie-nginx-module/archive/master.zip -o master.zip --silent
+unzip master.zip
+rm -rf master.zip
+
+
+cp /usr/local/src/centminmod/inc/nginx_upgrade.inc /usr/local/src/centminmod/inc/nginx_upgrade.inc.bak.$ran_it
+s='--with-http_gzip_static_module' ; r='--with-http_gzip_static_module --add-module=../testcookie-nginx-module-master'
+sed -i "s#$s#$r#g" /usr/local/src/centminmod/inc/nginx_upgrade.inc
+
+
+echo '
+
+	Successfully installed! Do not forget to run:
+
+"cd /usr/local/src/centminmod; ./centmin.sh" and "Option 4" to update!
+
+
+'
+
